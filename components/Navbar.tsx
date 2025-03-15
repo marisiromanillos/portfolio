77cff70bc0fbbb63e097c3c7569e58ae1bc99f65
marisiromanillos <marisiromanillos@gmail.com>
2025-03-15 12:27:45 +0000

    nav needs work

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,24 @@
-
-import React from 'react';
+import Link from "next/link";
 
 const Navbar = () => {
-    return <>
-    <div>navbar</div>
+  return (
+    <>
+      <header className="fixed top-0 left-0 w-full px-12 bg-slate-800 z-10 py-4 mx-auto shadow-lg">
+        <nav className="flex items-center justify-between max-w-7xl mx-auto">
+          <div className="text-white gap-2 text-2xl font-bold flex items-center justify-between max-w-7xl mx-auto">
+            <Link href="/" className="hover:text-gray-300 transition-colors">
+              MySite
+            </Link>
+            <Link href="/" className="hover:text-gray-300 transition-colors">
+              MySite
+            </Link>
+            <Link href="/" className="hover:text-gray-300 transition-colors">
+              MySite
+            </Link>
+          </div>
+        </nav>
+      </header>
     </>
-}
+  );
+};
 export default Navbar;
